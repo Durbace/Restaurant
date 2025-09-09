754fb28e44cbd9224a3051ca6c6166f9eaa32156
Durbaca <dianadurbaca2003@gmail.com>
2025-09-09 09:41:51 +0300

    feat: implement dropdown hover functionality in homepage component

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -4,8 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-homepage',
   imports: [],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
+  private hoverTimers = new WeakMap<HTMLElement, number>();
 
+  openHover(evt: Event) {
+    const el = evt.currentTarget as HTMLElement;
+    const t = this.hoverTimers.get(el);
+    if (t) {
+      clearTimeout(t);
+      this.hoverTimers.delete(el);
+    }
+    el.classList.add('open');
+  }
+
+  closeHover(evt: Event) {
+    const el = evt.currentTarget as HTMLElement;
+    const prev = this.hoverTimers.get(el);
+    if (prev) clearTimeout(prev);
+    const t = window.setTimeout(() => {
+      el.classList.remove('open');
+      this.hoverTimers.delete(el);
+    }, 120);
+    this.hoverTimers.set(el, t);
+  }
 }
